d2c732a6e6c463e2564786f52c46d71a2198e7a0
Marcel <mbertok@student.unimelb.edu.au>
2017-05-09 11:48:02 +1000

    Added csv support

--- Driver.java
+++ Driver.java
@@ -1,20 +1,25 @@
 package SWEN90004;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.io.IOException;
 
 public class Driver {
 	private World Map;
 	public int noOfEthnicities;
 	private double initialPtr;
 	private int immigrantsPerDay;
-	public Driver()
+    private int ticks;
+	public Driver(int t)
 	{
 		noOfEthnicities = 4;
 		initialPtr = 0.12;
 		immigrantsPerDay = 1;
 		Map  = new World();
+        ticks=t;
 	}
 	//Immigration Phase
 	public void Immigration()
@@ -54,13 +59,36 @@ public class Driver {
 	{
 		Map.Death();
 	}
-	public void drive() {
-		while (true)
+    public void Count(){
+        Map.count();
+    }
+    public String Status(){
+        int[] result=Map.export();
+        String s="";
+        for(int i=0;i<result.length;i++){
+            s=s+Integer.toString(result[i]);
+            if(i==result.length-1){
+                s=s+"\n";
+            }
+            else{
+                s=s+",";
+            }
+        }
+        return s;
+    }
+	public void drive() throws IOException{
+        FileWriter f=new FileWriter("ethno.csv");
+        f.write("Tick,CC,CD,DC,DD \n");
+		for(int i=1;i<=ticks;i++)
 		{
 			Immigration();
 			Interaction();
 			Reproduction();
 			Death();
+            Count();
+            f.write(i);
+            f.write(Status());
 		}
+        f.close();
 	}
 }
